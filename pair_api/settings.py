89ccbc06145a736a80ef587d27212d9f67838b2b
Ireoluwa <fakay96@gmail.com>
2026-02-16 19:48:20 +0000

    Update settings for PostgreSQL database configuration and add required packages to requirements.txt

--- pair_api/settings.py
+++ pair_api/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -73,14 +73,19 @@ WSGI_APPLICATION = 'pair_api.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME", "pair_takehome"),
+        "USER": os.environ.get("DB_USER", "pair"),
+        "PASSWORD": os.environ.get("DB_PASSWORD", "pair"),
+        "HOST": os.environ.get("DB_HOST", "localhost"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
+        "TEST": {
+            "NAME": os.environ.get("DB_NAME", "pair_takehome"),
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
